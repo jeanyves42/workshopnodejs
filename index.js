1f4aeeca9dbfa81fd780b8ef7f4e201af54037fd
jeanyves <jyb@batiactugroupe.com>
2020-05-25 13:23:02 +0200

    stockage dans variable du contenu de products

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let fs = require('fs')
 let path = require('path')
 
 fs.readFile(path.join(__dirname,'products.json'), 'utf8', (err, content) => {
-  console.log(content)
+  let products = content
 })
