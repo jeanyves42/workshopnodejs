fc5f37accf306eb250eb507e3c16751fa9b25bba
jeanyves <jyb@batiactugroupe.com>
2020-05-25 14:27:47 +0200

    orderProductById

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let fs = require('fs');
 let path = require('path');
 
-getAllProducts();
+orderProductById("P1");
 
-function getAllProducts() {
+function getAllProducts(cb) {
 
   fs.readFile(path.join(__dirname,'products.json'), 'utf8', (err, content) => {
     if(err) {
-      return console.log(err,'Erreur');
+      return cb(err);
     }
     try {
-      var products = JSON.parse(content).products;
+      var products = JSON.parse(content);
       if(products.length > 0) {
         console.log("Bienvenue. Voici les produits disponibles :");
     
@@ -18,11 +18,43 @@ function getAllProducts() {
           console.log('- ' + product.id + ' - ' + product.name + ' / ' + product.EUR_price/100 + ' / ' + product.orders_counter);
         });
       }
-      // console.log(products)
+      return cb(null, products);
     } catch(e) {
-      console.log(e, 'Erreur parsing');
+      return cb(e);
     }
   
   })
 
 }
+
+
+function orderProductById(id) {
+
+  getAllProducts((err, products) => {
+    if(err) {
+      return console.log(err,'Erreur');
+    }
+
+    var link = '';
+
+    products.forEach(product => {
+      if(product.id === id) {
+        product.orders_counter += 1;
+        link = product.file_link;
+      }
+
+    })
+
+    fs.writeFile(path.join(__dirname,'products.json'), JSON.stringify(products,null,4), 'utf8', (err) => {
+      if(err) {
+        return console.log(err);
+      }
+
+      console.log("Commande terminée. Voici votre fichier : " + link);
+
+    })
+
+  })
+
+}
+
