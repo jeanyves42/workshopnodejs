b28a5f090c5731f24a974047d5f086211b186584
jeanyves <jyb@batiactugroupe.com>
2020-05-25 13:51:58 +0200

    modification getAllProducts dans index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 let fs = require('fs')
 let path = require('path')
 
-fs.readFile(path.join(__dirname,'products.json'), 'utf8', (err, content) => {
-  if(err) {
-    return console.log(err,'Erreur')
-  }
-  try {
-    var products = JSON.parse(content).products
-    // console.log(products)
-  } catch(e) {
-    console.log(e, 'Erreur parsing')
-  }
+getAllProducts()
 
-  getAllProducts(products)
-})
+function getAllProducts() {
 
-function getAllProducts(products) {
-
-  console.log("Bienvenue. Voici les produits disponibles :")
-
-  products.forEach(product => {
-    console.log('- ' + product.id + ' - ' + product.name + ' / ' + product.EUR_price/100 + ' / ' + product.orders_counter)
-  });
+  fs.readFile(path.join(__dirname,'products.json'), 'utf8', (err, content) => {
+    if(err) {
+      return console.log(err,'Erreur')
+    }
+    try {
+      var products = JSON.parse(content).products
+      if(products.length > 0) {
+        console.log("Bienvenue. Voici les produits disponibles :")
+    
+        products.forEach(product => {
+          console.log('- ' + product.id + ' - ' + product.name + ' / ' + product.EUR_price/100 + ' / ' + product.orders_counter)
+        });
+      }
+      // console.log(products)
+    } catch(e) {
+      console.log(e, 'Erreur parsing')
+    }
+  
+  })
 
 }
