1772a45ecdceadd52121b5c61da740bbcc124259
jeanyves <jyb@batiactugroupe.com>
2020-05-25 13:53:28 +0200

    ajout ; dans code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let fs = require('fs')
-let path = require('path')
+let fs = require('fs');
+let path = require('path');
 
-getAllProducts()
+getAllProducts();
 
 function getAllProducts() {
 
   fs.readFile(path.join(__dirname,'products.json'), 'utf8', (err, content) => {
     if(err) {
-      return console.log(err,'Erreur')
+      return console.log(err,'Erreur');
     }
     try {
-      var products = JSON.parse(content).products
+      var products = JSON.parse(content).products;
       if(products.length > 0) {
-        console.log("Bienvenue. Voici les produits disponibles :")
+        console.log("Bienvenue. Voici les produits disponibles :");
     
         products.forEach(product => {
-          console.log('- ' + product.id + ' - ' + product.name + ' / ' + product.EUR_price/100 + ' / ' + product.orders_counter)
+          console.log('- ' + product.id + ' - ' + product.name + ' / ' + product.EUR_price/100 + ' / ' + product.orders_counter);
         });
       }
       // console.log(products)
     } catch(e) {
-      console.log(e, 'Erreur parsing')
+      console.log(e, 'Erreur parsing');
     }
   
   })
